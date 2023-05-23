b27a6fe5ccf1655e43c39c3102183c70610dff86
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-05-23 12:11:05 +0600

    Shared section title created

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -9,10 +9,15 @@ import slide2 from "../../../assets/home/slide2.jpg";
 import slide3 from "../../../assets/home/slide3.jpg";
 import slide4 from "../../../assets/home/slide4.jpg";
 import slide5 from "../../../assets/home/slide5.jpg";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const Category = () => {
   return (
-    <>
+    <section>
+      <SectionTitle
+        heading={"ORDER ONLINE"}
+        subHeading={"From 11:00am to 10pm"}
+      />
       <Swiper
         slidesPerView={4}
         spaceBetween={30}
@@ -51,7 +56,7 @@ const Category = () => {
           </h3>
         </SwiperSlide>
       </Swiper>
-    </>
+    </section>
   );
 };
 
