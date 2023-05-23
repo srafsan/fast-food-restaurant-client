38e7feb1d153d11ea6225ca36c7aa65ed4fa49b2
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-05-23 20:32:14 +0600

    Dynamic page title by route done

---
+++ src/pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet-async";
+
+const Menu = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Menu</title>
+      </Helmet>
+      <h2>Men Men Menu</h2>
+    </div>
+  );
+};
+
+export default Menu;
--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
   const navOptions = (
     <>
       <li>
-        <a>Item 1</a>
+        <Link to="/">Home</Link>
       </li>
       <li>
-        <a>Item 3</a>
+        <Link to="/menu">Menu</Link>
       </li>
     </>
   );
