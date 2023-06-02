75ff825d116d7d9460ad3a9ff841ae2ae0366514
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-06-02 09:08:01 +0600

    Make user API secure and verify Admin

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -9,12 +9,13 @@ import {
   FaUsers,
 } from "react-icons/fa";
 import useCart from "../hooks/useCart";
+import useAdmin from "../hooks/useAdmin";
 
 const Dashboard = () => {
   const [cart] = useCart();
 
   // TODO: load data from the server to have dynamic isAdmin based on Data
-  const isAdmin = true;
+  const [isAdmin] = useAdmin();
 
   return (
     <div className="drawer drawer-mobile">
--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,15 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
-import { useContext } from "react";
-import { AuthContext } from "../providers/AuthProvider";
 import useAxiosSecure from "./useAxiosSecure";
+import useAuth from "./useAuth";
 
 const useCart = () => {
-  const { user } = useContext(AuthContext);
-  const token = localStorage.getItem("access-token");
+  const { user, loading } = useAuth();
   const [axiosSecure] = useAxiosSecure();
 
   const { refetch, data: cart = [] } = useQuery({
     queryKey: ["carts", user?.email],
+    enabled: !loading,
     queryFn: async () => {
       const res = await axiosSecure(`/carts?email=${user?.email}`);
       return res.data;
